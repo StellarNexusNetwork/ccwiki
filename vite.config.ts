26250d825f5589f3d980b9ed33119b6852a7c148
漫画猫猫 <1144718922@qq.com>
2024-09-05 04:53:28 +0800

    updata config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cc.project.v4.web2/',
   plugins: [
     vue(),
     vueJsx(),
