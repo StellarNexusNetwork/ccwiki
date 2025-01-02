e4839d1a2f0de02f291adf3e7ab24d6b9b6eabe0
星韵猫猫 <NekoNoct@users.noreply.github.com>
2025-01-02 10:24:22 +0800

    refactor(public): 使用国际化文本替换硬编码字符串

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,7 @@ import zh_cn from './locales/zh_cn.json';
 import en_us from './locales/en_us.json';
 
 const i18n = createI18n({
-    locale: "en_us", // 设置当前语言，默认使用 "en"
+    locale: "zh_cn", // 设置当前语言，默认使用 "en"
     fallbackLocale: "zh_cn", // 定义后备语言，当当前语言缺少翻译时使用
     legacy: false, // 启用组合式 API 的写法
     globalInjection: true, // 全局注册 $t 方法以便在模板中使用
