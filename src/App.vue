58ecd27ea86f778c5ae68874fbade18c9d8652a2
星韵猫猫 <NekoNoct@users.noreply.github.com>
2025-03-27 10:53:28 +0800

    fix: 修复多处代码格式和样式问题

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <titleBar />
-  <navigationBar v-model:mainDivStyle="mainDivStyle" v-model:mainStyle="mainStyle" />
+  <titleBar/>
+  <navigationBar v-model:mainDivStyle="mainDivStyle" v-model:mainStyle="mainStyle"/>
   <div class="mainDiv" :style="mainDivStyle">
     <div class="main" :style="mainStyle">
-      <RouterView class="router-view" />
+      <RouterView class="router-view"/>
     </div>
   </div>
   <div class="routerLoading" :style="[routerLoadingS, { marginLeft: mainDivStyle.paddingLeft }]">
     <div class="loading_bg" :style="rt_loading_bgS">
-      <svg xmlns="http://www.w3.org/2000/svg" class="loading" :style="rt_loadingS" width="130" height="130" viewBox="0 0 130 130">
-        <circle cx="65" cy="65" r="60" stroke="#04AAEB" stroke-width="10" fill="none" class="circle" />
+      <svg xmlns="http://www.w3.org/2000/svg" class="loading" :style="rt_loadingS" width="130" height="130"
+           viewBox="0 0 130 130">
+        <circle cx="65" cy="65" r="60" stroke="#04AAEB" stroke-width="10" fill="none" class="circle"/>
       </svg>
     </div>
   </div>
@@ -18,10 +19,9 @@
 <script setup lang="ts">
 import titleBar from './components/titleBar.vue'
 import navigationBar from './components/navigationBar.vue'
-import { RouterView, useRouter } from 'vue-router'
-import { ref, watchEffect } from 'vue'
-import { useWindowStore } from '@/stores/window'
-
+import {RouterView, useRouter} from 'vue-router'
+import {ref, watchEffect} from 'vue'
+import {useWindowStore} from '@/stores/window'
 
 
 let ifLoadingFinish = false;
@@ -30,28 +30,32 @@ window.addEventListener('load', function () {
 });
 
 
-let mainDivStyle = ref({ paddingLeft: '50px' })
-let mainStyle = ref({ width: 'calc(100vw - 50px)', position: 'static' as 'static' | 'absolute' | 'relative' | 'fixed', right: 'auto' })
+let mainDivStyle = ref({paddingLeft: '50px'})
+let mainStyle = ref({
+  width: 'calc(100vw - 50px)',
+  position: 'static' as 'static' | 'absolute' | 'relative' | 'fixed',
+  right: 'auto'
+})
 
-let routerLoadingS = ref({ display: 'none' })
-let rt_loading_bgS = ref({ width: '100px', height: '100px', opacity: 0, marginBottom: '0px', transitionDuration: '0.5s' })
-let rt_loadingS = ref({ opacity: 0 })
+let routerLoadingS = ref({display: 'none'})
+let rt_loading_bgS = ref({width: '100px', height: '100px', opacity: 0, marginBottom: '0px', transitionDuration: '0.5s'})
+let rt_loadingS = ref({opacity: 0})
 let rt_isAnimating = false;
-let Allowrouting = false;
+let allowRouting = false;
 let rt_ae_f = false
 
 // 移动端适配
-let oldmainDivPL = '50px'
+let oldMainDivPL = '50px'
 let oldMainStyleW = 'calc(100vw - 50px)'
 watchEffect(() => {
   if (useWindowStore().windowWidth < 670 && mainDivStyle.value.paddingLeft != "0px") {
-    oldmainDivPL = mainDivStyle.value.paddingLeft
+    oldMainDivPL = mainDivStyle.value.paddingLeft
     oldMainStyleW = mainStyle.value.width
     mainDivStyle.value.paddingLeft = "0px";
     mainStyle.value.width = '100vw'
   }
   if (useWindowStore().windowWidth >= 670 && mainDivStyle.value.paddingLeft == "0px") {
-    mainDivStyle.value.paddingLeft = oldmainDivPL
+    mainDivStyle.value.paddingLeft = oldMainDivPL
     mainStyle.value.width = oldMainStyleW
   }
 })
@@ -59,20 +63,26 @@ watchEffect(() => {
 
 const router = useRouter()
 router.beforeEach((to, from, next) => {
-  if (ifLoadingFinish && rt_isAnimating == false) {
+  if (ifLoadingFinish && !rt_isAnimating) {
     rt_isAnimating = true;
-    Allowrouting = false;
-    Object.assign(rt_loading_bgS, { width: '100px', height: '100px', opacity: 0, marginBottom: '0px', transitionDuration: '0.5s' })
+    allowRouting = false;
+    Object.assign(rt_loading_bgS, {
+      width: '100px',
+      height: '100px',
+      opacity: 0,
+      marginBottom: '0px',
+      transitionDuration: '0.5s'
+    })
     routerLoadingS.value.display = 'none';
     rt_loadingS.value.opacity = 0;
     setTimeout(() => {
       routerLoadingS.value.display = 'flex';
     }, 10);
     setTimeout(() => {
-      Object.assign(rt_loading_bgS.value, { width: '250px', height: '250px', opacity: 1, marginBottom: '70px' });
+      Object.assign(rt_loading_bgS.value, {width: '250px', height: '250px', opacity: 1, marginBottom: '70px'});
     }, 20);
     setTimeout(() => {
-      Object.assign(rt_loading_bgS.value, { width: '200px', height: '200px', marginBottom: '0px' });
+      Object.assign(rt_loading_bgS.value, {width: '200px', height: '200px', marginBottom: '0px'});
     }, 500);
     setTimeout(() => {
       rt_loadingS.value.opacity = 1;
@@ -81,16 +91,16 @@ router.beforeEach((to, from, next) => {
       rt_loading_bgS.value.transitionDuration = '0.75s';
     }, 1499);
     setTimeout(() => {
-      Object.assign(rt_loading_bgS.value, { width: 'calc(100vw + 100vh)', height: 'calc(100vw + 100vh)' });
+      Object.assign(rt_loading_bgS.value, {width: 'calc(100vw + 100vh)', height: 'calc(100vw + 100vh)'});
     }, 1500);
     setTimeout(() => {
       next()
       rt_loading_bgS.value.transitionDuration = '0.5s';
-      Allowrouting = true;
+      allowRouting = true;
     }, 2250);
   } else {
     //这里是更改路由 但好像又失效了.
-    if (Allowrouting || ifLoadingFinish == false) {
+    if (allowRouting || !ifLoadingFinish) {
       next()
       rt_ae_f = true
     }
@@ -98,9 +108,9 @@ router.beforeEach((to, from, next) => {
 })
 
 router.afterEach(() => {
-  if (ifLoadingFinish && rt_ae_f == false) {
-    Allowrouting = false;
-    Object.assign(rt_loading_bgS.value, { width: '200px', height: '200px' });
+  if (ifLoadingFinish && !rt_ae_f) {
+    allowRouting = false;
+    Object.assign(rt_loading_bgS.value, {width: '200px', height: '200px'});
     setTimeout(() => {
       rt_loadingS.value.opacity = 0;
     }, 500);
@@ -136,7 +146,7 @@ router.afterEach(() => {
 
   .main {
     height: calc(100vh - 42px);
-    border-radius: 20px 0px 20px 0px;
+    border-radius: 20px 0 20px 0;
   }
 }
 
@@ -146,7 +156,7 @@ router.afterEach(() => {
   }
 
   .navigationBar {
-    bottom: 0px;
+    bottom: 0;
     height: 50px;
   }
 
@@ -198,8 +208,8 @@ router.afterEach(() => {
   justify-content: center;
   align-items: center;
   position: fixed;
-  bottom: 0px;
-  border-radius: 20px 0px 20px 0px;
+  bottom: 0;
+  border-radius: 20px 0 20px 0;
   overflow: hidden;
   transition-duration: 0.5s;
 }
@@ -210,7 +220,7 @@ router.afterEach(() => {
   align-items: center;
   background: #fafafa;
   border-radius: 50%;
-  box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.3);
+  box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.3);
   flex-shrink: 0;
 }
 
