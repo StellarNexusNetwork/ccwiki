58fc525e80f8a9d5cbb1bae9c70d979f3024c1ba
星韵猫猫 <NekoNoct@users.noreply.github.com>
2025-03-27 10:53:28 +0800

    fix: 修复多处代码格式和样式问题

--- src/views/20241108/eye8_copy.vue
+++ src/views/20241108/eye8_copy.vue
@@ -1,78 +1,77 @@
 <template>
-    <div class="main2">
-        <div class="body"></div>
-    </div>
+  <div class="main2">
+    <div class="body"></div>
+  </div>
 </template>
 
 <script setup lang="ts">
 
-import ApexCharts from 'vue3-apexcharts'
-import { ref, onMounted, onUnmounted } from 'vue'
+import {onMounted} from 'vue'
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 onMounted(() => {
-    const Dbody = document.querySelector('.body');
+  const Dbody = document.querySelector('.body');
 
-    const scene = new THREE.Scene()
-    scene.fog = new THREE.Fog(0x999999, 1, 200);
-    scene.background = new THREE.Color(0x999999);
+  const scene = new THREE.Scene()
+  scene.fog = new THREE.Fog(0x999999, 1, 200);
+  scene.background = new THREE.Color(0x999999);
 
-    const camera = new THREE.PerspectiveCamera(
-        45,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        1000
-    );
+  const camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+  );
 
 
-    const renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  Dbody!.appendChild(renderer.domElement);
+
+
+  // const geometry = new THREE.BoxGeometry(1, 1, 1);
+  // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  // const cube = new THREE.Mesh(geometry, material);
+  // scene.add(cube);
+
+  const gltfLoader = new GLTFLoader();
+
+  gltfLoader.load('./static/20241108/model/dg-lab-v3.glb', (gltf) => {
+    console.log(gltf)
+    scene.add(gltf.scene)
+  })
+
+  camera.position.z = 5;
+  camera.lookAt(0, 0, 0)
+
+  const axisHelper = new THREE.AxesHelper(5);
+  scene.add(axisHelper);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.025;
+
+  function animate() {
+    controls.update();
+    requestAnimationFrame(animate);
+
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+
+  }
+
+  animate();
+
+  window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
-    Dbody!.appendChild(renderer.domElement);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-
-    // const geometry = new THREE.BoxGeometry(1, 1, 1);
-    // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    // const cube = new THREE.Mesh(geometry, material);
-    // scene.add(cube);
-
-    const gltfLoader = new GLTFLoader();
-
-    gltfLoader.load('./static/20241108/model/dg-lab-v3.glb', (gltf) => {
-        console.log(gltf)
-        scene.add(gltf.scene)
-    })
-
-    camera.position.z = 5;
-    camera.lookAt(0, 0, 0)
-
-    const axisHelper = new THREE.AxesHelper(5);
-    scene.add(axisHelper);
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.025;
-
-    function animate() {
-        controls.update();
-        requestAnimationFrame(animate);
-
-        // cube.rotation.x += 0.01;
-        // cube.rotation.y += 0.01;
-        renderer.render(scene, camera);
-
-    }
-    animate();
-
-    window.addEventListener('resize', () => {
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-
-    })
+  })
 })
-
 
 
 </script>
--- src/views/chat/AutoResizeTextarea.vue
+++ src/views/chat/AutoResizeTextarea.vue
@@ -1,49 +1,48 @@
 <template>
-    <textarea class="auto-resize-textarea" v-model="content" @input="autoResize" rows="1"></textarea>
+  <textarea class="auto-resize-textarea" v-model="content" @input="autoResize" rows="1"></textarea>
 </template>
 
 <script>
 export default {
-    name: 'AutoResizeTextarea',
-    props: {
-        modelValue: String,
+  name: 'AutoResizeTextarea',
+  props: {
+    modelValue: String,
+  },
+  emits: ['update:modelValue', 'update-height'], // 定义新的 'update-height' 事件
+  data() {
+    return {
+      content: this.modelValue,
+    };
+  },
+  watch: {
+    modelValue(newVal) {
+      this.content = newVal;
     },
-    emits: ['update:modelValue', 'update-height'], // 定义新的 'update-height' 事件
-    data() {
-        return {
-            content: this.modelValue,
-        };
+  },
+  methods: {
+    autoResize(event) {
+      const textarea = event.target;
+      textarea.style.height = 'auto'; // 先重置高度
+      textarea.style.height = `${textarea.scrollHeight}px`; // 动态设置高度
+      this.$emit('update:modelValue', this.content);
+      this.$emit('update-height', textarea.scrollHeight); // 将高度传递给父组件
     },
-    watch: {
-        modelValue(newVal) {
-            this.content = newVal;
-        },
-    },
-    methods: {
-        autoResize(event) {
-            const textarea = event.target;
-            textarea.style.height = 'auto'; // 先重置高度
-            textarea.style.height = `${textarea.scrollHeight}px`; // 动态设置高度
-            this.$emit('update:modelValue', this.content);
-            this.$emit('update-height', textarea.scrollHeight); // 将高度传递给父组件
-        },
-    },
+  },
 };
 </script>
 
 <style scoped>
 .auto-resize-textarea {
-    width: calc(100% - 60px);
-    min-height: 38px;
-    max-height: 300px;
-    resize: none;
-    padding: 5px;
-    font-size: 18px;
-    font-family: MiSans;
-    box-sizing: border-box;
-    background: rgba(0, 0, 0, 0);
-    border: none;
-    resize: none;
-    outline: none;
+  width: calc(100% - 60px);
+  min-height: 38px;
+  max-height: 300px;
+  padding: 5px;
+  font-size: 18px;
+  font-family: MiSans;
+  box-sizing: border-box;
+  background: rgba(0, 0, 0, 0);
+  border: none;
+  resize: none;
+  outline: none;
 }
 </style>
