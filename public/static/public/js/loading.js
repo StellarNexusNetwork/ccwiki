2e72b9950f39997cc4b3954afe043d5956e2bb30
星韵猫猫 <NekoNoct@users.noreply.github.com>
2025-04-03 21:26:24 +0800

    feat(public): add loading animation

--- public/static/public/js/loading.js
+++ public/static/public/js/loading.js
@@ -1,5 +1,5 @@
 let init_loading_bg = document.querySelector(".init_loading_bg");
-let init_loadingImg = document.querySelector(".init_loadingImg");
+let init_dots = document.querySelectorAll(".dot");
 let init_loading_logo = document.querySelector(".init_loading_logo");
 let options = document.querySelector(".options");
 let loading_poweredBy = document.querySelector(".poweredBy");
@@ -9,18 +9,12 @@ window.onload = () => {
     var id = setTimeout(function function_name() {
         init_loading_logo.style.opacity = "0";
         loading_poweredBy.style.opacity = "0";
-    }, 500);
+    }, 600);
     var id = setTimeout(function function_name() {
-        init_loadingImg.style.border = '10px solid #04AAEB';
-    }, 1000);
-    var id = setTimeout(function function_name() {
-        init_loadingImg.style.animation = "";
-        init_loadingImg.style.transform = " rotate(0deg)";
-        init_loadingImg.style.width = "100%";
-        init_loadingImg.style.height = "100%";
-        init_loadingImg.style.borderRadius = "5px";
-        init_loadingImg.style.border = '5px solid #57d4ff';
-    }, 1500);
+        init_dots.forEach(dot => {
+            dot.style.opacity = "0";
+        });
+    }, 1200);
     var id = setTimeout(function function_name() {
         init_loading_bg.style.transitionDuration = "0.5s";
         init_loading_bg.style.opacity = "0";
