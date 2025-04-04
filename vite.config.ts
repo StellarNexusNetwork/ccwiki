afaeb1913ad669dc8430b107dd915019a2ca3aef
星韵猫猫 <NekoNoct@users.noreply.github.com>
2025-04-04 20:10:11 +0800

    refactor(vite.config.ts): 优化构建配置，添加rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,38 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: process.env.NODE_ENV === 'production' ? '/cc.wiki.project.v4.web2/' : '/',
-  plugins: [
-    vue(),
-    vueJsx(),
-    // vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    // base: process.env.NODE_ENV === 'production' ? '/cc.wiki.project.v4.web2/' : '/',
+    plugins: [
+        vue(),
+        vueJsx(),
+        // vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/js/[name]-[hash].js',
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                assetFileNames: 'assets/css/[name]-[hash].css',
+            }
+        }
     }
-  }
-  // },
-  // devServer: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://127.0.0.1:5000',
-  //       changeOrigin: true,
-  //     }
-  //   }
-  // }
+    // },
+    // devServer: {
+    //   proxy: {
+    //     '/api': {
+    //       target: 'http://127.0.0.1:5000',
+    //       changeOrigin: true,
+    //     }
+    //   }
+    // }
 })
