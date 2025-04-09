e5469888d681c48bdc2b4b1cebf4c8186c3af4f4
星韵猫猫 <NekoNoct@users.noreply.github.com>
2025-04-09 22:42:09 +0800

    fix(setting/theme): 修复主题设置标题翻译路径

--- src/components/setting/theme.vue
+++ src/components/setting/theme.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="title">{{ $t("public.setting.theme_O.appearance.title") }}</div>
+  <div class="title">{{ $t("public.setting.themeDetail.appearance.title") }}</div>
   <div class="optionList">
     <div class="optionBox" v-for="(item, index) in appearanceOptions">
       <div class="option" :id="index === 0 ? 'firstItem' : undefined" @click="setAppearance(index)" :style="appearanceActive[index]">
