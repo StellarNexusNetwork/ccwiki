f68a801138b559c709b4ed0917db070e204753e1
星韵猫猫 <NekoNoct@users.noreply.github.com>
2025-06-14 10:48:12 +0800

    refactor: 重命名组件并优化代码结构

--- src/views/classification/CategoryList.vue
+++ src/views/classification/CategoryList.vue
@@ -1,16 +1,16 @@
 <template>
-  <div v-for="([key,value], index) in entries">
+  <div v-for="([key,value], index) in entries" :key="value.path">
     <div class="line" :id="index === 0 ? 'firstItem' : undefined"></div>
     <div class="boxDiv">
       <div class="title">{{ $t("docs." + value.path + ".title") }}</div>
       <div class="itemList">
-        <button class="item" :id="index === 0 ? 'firstItem' : undefined" @click="routePush('/classification/'+value.path+'/'+item.path)" v-for="(item, index) in value.items">
+        <button class="item" :id="index === 0 ? 'firstItem' : undefined" @click="routePush('/classification/'+value.path+'/'+item.path)" v-for="(item, index) in value.items" :key="item.path">
           <div class="title" :style="{ 'viewTransitionName': 'class-itemList-title-' + value.path + '-' + item.path}">
             {{ $t("docs." + value.path + ".items." + item.path + ".title") }}
           </div>
           <div class="introduction">{{ $t("docs." + value.path + ".items." + item.path + ".content") }}</div>
           <div class="iconList">
-            <img class="icon" :src="icon_value.iconSrc" alt="SVG Image" draggable="false" v-for="([icon_key,icon_value], index) in Object.entries(iconList[value.path][item.path]) as [string, any][]" :style="{ 'viewTransitionName': 'class-item-img-' + value.path + '-' + item.path + '-' + icon_value.id }">
+            <img class="icon" :src="icon_value.iconSrc" alt="SVG Image" draggable="false" v-for="([icon_key,icon_value], index) in Object.entries(iconList[value.path][item.path]) as [string, any][]" :style="{ 'viewTransitionName': 'class-item-img-' + value.path + '-' + item.path + '-' + icon_value.id }" :key="index">
           </div>
         </button>
       </div>
@@ -18,28 +18,28 @@
   </div>
 </template>
 <script setup lang="ts">
-import {useDataSourcesStore} from "@/stores/dataSources";
-import {useSettingStore} from "@/stores/setting";
+import {useDataSourcesStore} from '@/stores/dataSources';
+import {useSettingStore} from '@/stores/setting';
 import {useI18n} from 'vue-i18n';
-import {computed, toRaw} from "vue";
+import {computed, toRaw} from 'vue';
 import {useRouter} from 'vue-router';
 import get from 'lodash/get';
 
 const router = useRouter();
 
-const lang = computed(() => useSettingStore().setting.lang)
-const routes = computed(() => useDataSourcesStore().routeGroups)
+const lang = computed(() => useSettingStore().setting.lang);
+const routes = computed(() => useDataSourcesStore().routeGroups);
 
 // 刷新数据
 if (Object.keys(useDataSourcesStore().localRepositoriesData).length === 0) {
-  await useDataSourcesStore().refreshData()
+  await useDataSourcesStore().refreshData();
 
   // 合并语言数据
   const {getLocaleMessage} = useI18n();
 
-  const updateLang = await useDataSourcesStore().mergeLangData(getLocaleMessage)
+  const updateLang = await useDataSourcesStore().mergeLangData(getLocaleMessage);
   for (const lang in updateLang) {
-    useI18n().setLocaleMessage(lang, updateLang[lang])
+    useI18n().setLocaleMessage(lang, updateLang[lang]);
   }
 }
 
@@ -48,45 +48,45 @@ const entries = computed(() => {
   return Object.entries(obj) as [string, any][];
 });
 
-const iconList: Record<string, any> = await getAllIconList(entries.value)
+const iconList: Record<string, any> = await getAllIconList(entries.value);
 
 async function getAllIconList(entries: any) {
-  let iconList = {}
+  let iconList = {};
   for (const [key, value] of entries) {
     for (const item in toRaw(value).items) {
-      useDataSourcesStore().deepSet(iconList, [value.path, item], await getIconList(value.path, item))
+      useDataSourcesStore().deepSet(iconList, [value.path, item], await getIconList(value.path, item));
     }
   }
-  return iconList
+  return iconList;
 }
 
 async function getIconList(category: string, subcategory: string) {
-  let iconList = []
-  const group = get(useDataSourcesStore().localRepositoriesData, [Object.keys(routes.value)[0], 'docs', useSettingStore().setting.lang, category, subcategory])
+  let iconList = [];
+  const group = get(useDataSourcesStore().localRepositoriesData, [Object.keys(routes.value)[0], 'docs', useSettingStore().setting.lang, category, subcategory]);
   if (group) {
     const first3 = Object.fromEntries(Object.entries(group).slice(0, 3));
     for (const [key, value] of Object.entries(first3)) {
-      const item = await useDataSourcesStore().getOrCacheItem([Object.keys(routes.value)[0], 'docs', useSettingStore().setting.lang, category, subcategory, key])
-      item['id'] = key
-      iconList.push(item)
+      const item = await useDataSourcesStore().getOrCacheItem([Object.keys(routes.value)[0], 'docs', useSettingStore().setting.lang, category, subcategory, key]);
+      item['id'] = key;
+      iconList.push(item);
     }
 
     // 图标不足时补齐
     // for (let i = 0; i < 3 - Object.keys(first3).length; i++) {
     //   iconList.push({'iconSrc': '/static/public/svg/NotFound.svg'})
     // }
-    return iconList
+    return iconList;
   } else {
     return [
       {'iconSrc': '/static/public/svg/NotFound.svg'},
       {'iconSrc': '/static/public/svg/NotFound.svg'},
       {'iconSrc': '/static/public/svg/NotFound.svg'}
-    ]
+    ];
   }
 }
 
 function routePush(url: string) {
-  router.push(url)
+  router.push(url);
 }
 </script>
 <style scoped>
