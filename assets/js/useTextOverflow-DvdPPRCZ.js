b2315c9780798b8c984394857b8d72e5eaaccc52
NekoNoct <NekoNoct@users.noreply.github.com>
2025-06-16 06:28:09 +0000

    deploy: 34e0fc521d3d20ebfe4f3e74b848e436d2e30127

---
+++ assets/js/useTextOverflow-DvdPPRCZ.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-cFoAmasS.js";function n(){const e=t(!1);function o(){e.value=!0}function r(){e.value=!1}return{shouldAddGap:e,onOverflowDetected:o,onOverflowCleared:r}}export{n as u};
