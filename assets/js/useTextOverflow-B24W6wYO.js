aa519972a3ea7656a0dd385407e9d5316e177293
NekoNoct <NekoNoct@users.noreply.github.com>
2025-07-19 16:12:35 +0000

    deploy: a306849bbdf045d284bec8ba0839251eb1837275

---
+++ assets/js/useTextOverflow-B24W6wYO.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-TG1NFP9K.js";function o(){const o=e(!1);return{shouldAddGap:o,onOverflowDetected:function(){o.value=!0},onOverflowCleared:function(){o.value=!1}}}export{o as u};
