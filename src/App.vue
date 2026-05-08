8c7c7b9b3664f12fd79c2b55151129fda393e343
星韵猫猫 <NekoNoct@users.noreply.github.com>
2026-05-08 13:39:22 +0800

    feat(theme): 支持系统暗色模式监听

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ import SettingDialog from "@/components/settings/SettingDialog.vue";
 import {useWindowStore} from '@/stores/window';
 import {useDataSourcesStore} from '@/stores/dataSources';
 import {RouterView, useRouter} from 'vue-router';
-import {computed, ref, watchEffect} from 'vue';
+import {computed, onMounted, onUnmounted, ref, watchEffect} from 'vue';
 import {useRouteTransition} from '@/composables/useRouteTransition';
 import {useSettingStore} from '@/stores/setting';
 
@@ -72,12 +72,28 @@ const {routerLoadingS, rtLoadingBgS, rtLoadingS} = useRouteTransition(router, {
 const setting = useSettingStore().setting;
 const theme = setting.theme;
 
+const media = window.matchMedia('(prefers-color-scheme: dark)')
+
+const systemDark = ref(media.matches)
+
+const updateSystemTheme = (e: MediaQueryListEvent) => {
+  systemDark.value = e.matches
+}
+
+onMounted(() => {
+  media.addEventListener('change', updateSystemTheme)
+})
+
+onUnmounted(() => {
+  media.removeEventListener('change', updateSystemTheme)
+})
+
 const isDark = computed(() => {
   return (
     theme.appearance === 'dark' ||
     (
       theme.appearance === 'auto' &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches
+      systemDark.value
     )
   )
 })
